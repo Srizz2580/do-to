4dad63149ea35cc7c5d58dc35cf131abe6e60374
Srizz2580 <srizz2580@gmail.com>
2022-08-05 19:50:54 +0545

    More functions

--- main.py
+++ main.py
@@ -1,60 +1,64 @@
 import curses
-from curses import wrapper
-from curses.textpad import Textbox, rectangle
+import time, json
 
-todo_list = [
-    "Hello",
-    "There",
-    "XD INIT"
-]
+ # Screen Funtions #
 
 def todo_view(stdscr):
-    stdscr.clear()
-    for i in range(0, len(todo_list)):
-        stdscr.addstr(f"[ - ]: {todo_list[i]}\n")
-    stdscr.refresh()
+    data = load(0)
 
-def get_input(stdscr):
-
-    inp = curses.newwin(8,55, 0,0)
-    inp.addstr(1,1, "TODO:")
-    
-    sub = inp.subwin(3, 41, 2, 1)
-    sub.border()
-
-    sub2 = sub.subwin(1, 40, 3, 2)
-    tb = Textbox(sub2)
-
-    inp.refresh()
-    tb.edit()
-    text = tb.gather().strip().replace('\n', '')
-
-    if text[len(text) - 1] == "x":
-        text = text[:-1]
-
-
-    return text
+    for i in range(0, len(data)):
+        stdscr.addstr(f" [ - ]: {data[i]}\n")   
 
 
 
-
-
-
+# \\ MAIN FUNCTION // #
 def main(stdscr):
+   
+    stdscr = curses.initscr()
     
     while True:
+        stdscr.clear()
         stdscr.refresh()
 
-        todo_view(stdscr)
+        todo_view(stdscr)   
 
-        pressed_key = chr(stdscr.getch())
-        
-        if pressed_key == 'a':
-            inp = get_input(stdscr)
-            todo_list.append(inp)
-        elif pressed_key == 'q':
-            break 
+        key = stdscr.getkey()
+
+        if key == 'q' or key == 'Q':
+            break
         else:
             continue
 
-wrapper(main)
+
+
+# JSON load and deload functions #
+def load(mode): 
+
+    if mode == 0:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+            data = data["todos"]
+    else:
+         with open("data.json", "r") as f:
+            data = json.load(f)
+            data = data["done"]
+
+    return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
