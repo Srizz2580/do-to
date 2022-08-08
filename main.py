0f4105df21281a626bf71425edd6fc92fb4aa7b7
Srizz2580 <srizz2580@gmail.com>
2022-08-08 19:23:19 +0545

    Added border

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 import curses
 import time, json
 
- # Screen Funtions #
+
+TODOS = []
+
+# Screen Funtions #
 def all_view(stdscr):
+    stdscr.refresh()
     y, x = stdscr.getmaxyx()
 
-    todo_win = curses.newwin(100, 50, 50, 50)
-    # done_win = curses.newwin(curses.LINES, curses.COLS)
+    todos = []
 
-    todo_win.border()
-    # done_win.border()
+    if len(TODOS) < 1:
+        todos = load(0)
+    else:
+        todos = TODOS
+    
+    height = int(len(todos) * 4)
+    to_y = y // 25
+    to_x = x // 50
 
-    todos = load(0)
-    done = load(1)
 
+    win = curses.newwin(height, 50, to_y, to_x)
+    win.clear()
+ 
     for i in range(0, len(todos)):
-        todo_win.addstr(f"[ - ]: {todos[i]}\n")
+        TODOS.append(todos[i])
+        win.addstr(f" [ - ]: {todos[i]}\n")
+    
+    win.border()
+    win.refresh()
+    del win
 
-    # for i in range(0, len(done)):
-        # done_win.addstr(f"[ X ]: {done[i]}\n")
-    stdscr.border() 
-    todo_win.refresh()
-    # done_win.refresh()
+def add_todo(stdscr):
+    y, x = stdscr.getmaxyx()
 
+    to_y = y // 10
+    to_x = x // 10
+
+    win = curses.newwin(50, 100, to_y, to_x)
+    win.erase()
+    
+    curses.echo(True)
+    win.addstr(1, 2, "Input todo: ")
+    
+    inpt = win.getstr(2, 2, 2000)
+
+    win.border()
+    win.refresh()
+
+    curses.echo(False)
+    return inpt
 
 # \\ MAIN FUNCTION // #
 def main(stdscr):
@@ -32,6 +60,7 @@ def main(stdscr):
     while True:
         stdscr.clear()
         stdscr.refresh()
+    
 
         all_view(stdscr)
         stdscr.refresh()
@@ -40,6 +69,9 @@ def main(stdscr):
 
         if key == 'q' or key == 'Q':
             break
+        elif key == 'a' or key == 'A':
+            inp = str(add_todo(stdscr)).replace("b", "").replace("'", "")
+            TODOS.append(inp)
         else:
             continue
 
@@ -58,12 +90,6 @@ def load(mode):
             data = data["done"]
 
     return data
-
-
-
-
-
-
 
 
 
