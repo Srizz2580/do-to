3ad515d6ebd69905eaac166161065170bc4b39de
Srizz2580 <srizz2580@gmail.com>
2022-08-14 17:03:55 +0545

    Bugs fixed

--- main.py
+++ main.py
@@ -107,6 +107,49 @@ def load_todo(stdscr):
     menu = data["todos"]
 
 
+# EDIT
+def edit_todo(stdscr, selected_row):
+    global menu
+
+    for idx, row in enumerate(menu):
+        if idx == selected_row:
+            stdscr.clear()
+            h, w = stdscr.getmaxyx()
+
+            x = (w // 2) - 30
+            y = (h // 2) - 5
+
+            curses.echo(True)
+
+            win = curses.newwin(10, 60, y, x)
+            win.refresh()
+
+
+            win.border()
+            win.refresh()
+
+            win.addstr(2, 2, "Edit the todo: ", curses.A_UNDERLINE)
+
+            win.addstr(4, 2, "+>", curses.A_BOLD)
+
+            win.refresh()
+            inp = win.getstr(4, 5, 2000)
+
+            curses.echo(False)
+            if str(inp) == "b''": return
+
+            else: 
+                xd = str(inp)
+                xd = xd[2:-1].strip()
+                if xd != "n":
+                    menu.pop(idx)
+                    menu.insert(idx, xd)
+
+                    stdscr.refresh()
+
+                else: return
+
+
 
 # // MAIN // #
 def main(stdscr):
@@ -140,5 +183,6 @@ def main(stdscr):
         if chr(key) == 'd' or chr(key) == 'D': del_todo(stdscr, menu, curr_row)
         if chr(key) == 's' or chr(key) == 'S': save_todo(stdscr)
         if chr(key) == 'l' or chr(key) == 'L': load_todo(stdscr)
+        if chr(key) == 'e' or chr(key) == 'E': edit_todo(stdscr, curr_row)
 
 curses.wrapper(main)
